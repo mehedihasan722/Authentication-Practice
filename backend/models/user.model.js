2f5c29239ee4710fa1ca3d61a770dae810366204
Mehedi <mjimehedi99@gmail.com>
2025-01-23 10:01:45 +0600

    🎉 Mongoose User Schema Completed
    gagdf

---
+++ backend/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    lastLogin: { type: Date, default: Date.now },
+    isVerified: { type: Boolean, default: false },
+    resetPasswordToken: { type: String },
+    resetPasswordExpiresAt: { type: Date },
+    verficationToken: { type: String },
+    verficationTokenExpiresAt: { type: Date },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
+
+// crate at and update at fields
